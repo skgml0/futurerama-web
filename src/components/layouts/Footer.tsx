3c5adac5ebcf3512df96b68ccbe52b3c0736e397
skgml0 <hnh2714@gmail.com>
2021-12-28 17:59:45 +0900

    ✨ Footer 추가

---
+++ src/components/layouts/Footer.tsx
@@ -0,0 +1,9 @@
+import styled from "@emotion/styled";
+import React from "react";
+import { Navigation } from ".";
+
+export const Footer=() => {
+  return ( 
+    <footer>Built by @ LaaHee</footer>
+  );
+};
