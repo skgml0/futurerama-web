e1ff4fee886380ce4c991b4628b8deb66256944b
skgml0 <hnh2714@gmail.com>
2021-12-28 10:11:31 +0900

    ✨  Navigation-path 연결

---
+++ src/constants/index.tsx
@@ -0,0 +1,77 @@
+export const INFO_API_ENDPOINT = "https://api.sampleapis.com/futurama/info";
+export const CHARACTERS_API_ENDPOINT =
+  "https://api.sampleapis.com/futurama/characters";
+export const CAST_API_ENDPOINT = "https://api.sampleapis.com/futurama/cast";
+export const EPISODE_API_ENDPOINT =
+  "https://api.sampleapis.com/futurama/episodes";
+
+export const ROUTES = [
+  {
+    ID: 0,
+    PATH: "/info",
+    LABEL: "Info",
+  },
+  {
+    ID: 1,
+    PATH: "/characters",
+    LABEL: "characters",
+    // SUBS: [
+    //   {
+    //     ID: 0,
+    //     PATH: "/dessert",
+    //     LABEL: "디저트 와인",
+    //     ORDER: 0,
+    //   },
+    //   {
+    //     ID: 1,
+    //     PATH: "/port",
+    //     LABEL: "포트 와인",
+    //     ORDER: 1,
+    //   },
+    //   {
+    //     ID: 2,
+    //     PATH: "/reds",
+    //     LABEL: "레드 와인",
+    //     ORDER: 2,
+    //   },
+    //   {
+    //     ID: 3,
+    //     PATH: "/rose",
+    //     LABEL: "로즈 와인",
+    //     ORDER: 3,
+    //   },
+    //   {
+    //     ID: 4,
+    //     PATH: "/sparkling",
+    //     LABEL: "스파클링 와인",
+    //     ORDER: 4,
+    //   },
+    //   {
+    //     ID: 5,
+    //     PATH: "/whites",
+    //     LABEL: "화이트 와인",
+    //     ORDER: 5,
+    //   },
+    // ],
+  },
+  {
+    ID: 2,
+    PATH: "/cast",
+    LABEL: "cast",
+  },
+  {
+    ID: 3,
+    PATH: "/episodes",
+    LABEL: "episodes",
+  },
+  {
+    ID: 4,
+    PATH: "/questions",
+    LABEL: "Questions",
+  },
+  {
+    ID: 5,
+    PATH: "/invent",
+    LABEL: "Invent",
+  },
+];
