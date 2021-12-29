e89f1d4c4f7ae10f7092934afe54d0fb2ae79cf4
skgml0 <hnh2714@gmail.com>
2021-12-29 16:27:13 +0900

    💄  CAST UI 변경

--- src/pages/cast/index.tsx
+++ src/pages/cast/index.tsx
@@ -21,14 +21,16 @@ const CastIndexPage: NextPage = () => {
           <Card key={`futurama-cast-${id}`}>
             {/* <Profile src={images.main} alt="" /> */}
             {/* <Person src= {imgArr[id-1]} alt="" /> */}
-            <p>{name}</p>
-            <p>{born}</p>
+            <E_Title>{name}</E_Title>
+            <Born>{born}</Born>
           </Card>
         );
       })}
     </Container>
   );
 };
+export default CastIndexPage;
+
 const Container = styled.div`
   display: grid;
   grid-template-columns: repeat(3, 1fr);
@@ -50,21 +52,25 @@ const Card = styled.div`
   flex-direction: column;
   justify-items: center;
   align-items: center;
-  height: 300px;
+  height: 100px;
   width: 360px;
   text-align: center;
   background-color: #f7f7c9;
   color: #919178;
   border: 1.5px solid #ffe;
   border-radius: 170px;
-  margin-top: 5%;
+  margin-top: 10%;
 `;
-const Person = styled.img`
-  height: 200px;
-  /* aspect-ratio: 9 / 16; */
-  object-fit: contain;
-  border: 2px solid #c9c0c0;
-  border-radius: 25%;
-  margin-top: 10px;
-`;
-export default CastIndexPage;
+
+const E_Title = styled.h2`
+    font-size: 22px;
+    color: black;
+    font-weight: 600;
+    margin: 5%;
+`
+const Born = styled.p`
+    font-size: 16px;
+    color: #464242;
+    margin-bottom: 5%;
+`
+
