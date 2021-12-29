ede43b8808906c3b13286b4f041c5cae652f1beb
skgml0 <hnh2714@gmail.com>
2021-12-29 15:20:27 +0900

    ⚡️  미디어쿼리 히든 속성 추가

--- src/components/layouts/Navigation.tsx
+++ src/components/layouts/Navigation.tsx
@@ -13,7 +13,7 @@ export const Navigation = () => {
   return (
     <Header>
       <h1>
-        <Link href='/'>
+        <Link href="/">
           <a>
             <Logoimg src="/futurama-logo.png" alt="futurama logo" />
           </a>
@@ -55,31 +55,36 @@ const Header = styled.header`
   justify-content: space-around;
   border-bottom: 2px solid #eee;
   background-color: black;
-`
+`;
 const Logoimg = styled.img`
-  height: 65px;
-  padding: 1% 5%;
-  
-`
+  max-height: 65px;
+  padding: 1%;
+`;
 const Sidenav = styled.ul`
   display: flex;
   gap: 10px;
   right: 30px;
   justify-content: center;
   align-items: center;
-  
-  color: linear-gradient(180deg, rgba(2,0,36,1) 0%, rgba(255,4,4,1) 31%);
+
+  color: linear-gradient(180deg, rgba(2, 0, 36, 1) 0%, rgba(255, 4, 4, 1) 31%);
 `;
 
 const Amenu = styled.a`
   cursor: pointer;
   font-size: 22px;
   font-weight: bold;
-  background: linear-gradient(to top, #b90606 30%, #251a1a );
+  background: linear-gradient(to top, #b90606 30%, #251a1a);
   -webkit-background-clip: text;
   -webkit-text-fill-color: transparent;
   -webkit-text-stroke: 0.3px yellow;
   padding: 10px;
   margin-right: 20px;
   line-height: 65px;
-`
+  @media screen and (max-width: 1200px) {
+    font-size: 14px;
+  }
+  @media screen and (max-width: 800px) {
+    display: none;
+  }
+`;
